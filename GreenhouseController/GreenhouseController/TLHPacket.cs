b5c3347857ef887b49a1f3e1b36b5104fee3fb63
Aaron Roby <aroby@ycp.edu>
2017-03-19 11:44:35 -0400

    Added data structures for the sensing team's packets

---
+++ GreenhouseController/GreenhouseController/TLHPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenhouseController
+{
+    public class TLHPacket
+    {
+        public int Type;
+        public int ID;
+        public int Temperature;
+        public int Light;
+        public int Humidity;
+    }
+}
