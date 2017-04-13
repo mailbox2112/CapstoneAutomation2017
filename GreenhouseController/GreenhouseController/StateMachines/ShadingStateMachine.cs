b0bc55fadcb09b52144fa301c51a08e78c667670
Aaron Roby <aroby@ycp.edu>
2017-04-13 14:17:52 -0400

    Refactored shading state machine as well as added unit tests

--- GreenhouseController/GreenhouseController/StateMachines/ShadingStateMachine.cs
+++ GreenhouseController/GreenhouseController/StateMachines/ShadingStateMachine.cs
@@ -38,6 +38,93 @@ namespace GreenhouseController.StateMachines
         }
 
         /// <summary>
+        /// Determines the state of the shading state machine based on lighting sensor value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public GreenhouseState DetermineState(double value)
+        {
+            // TODO: revamp!
+            if (CurrentState == GreenhouseState.SHADING)
+            {
+                CurrentState = GreenhouseState.PROCESSING_SHADING;
+            }
+            else
+            {
+                CurrentState = GreenhouseState.PROCESSING_DATA;
+            }
+
+            // If we don't have a manual shading command
+            if (ManualShade == null)
+            {
+                // If we're coming from the wait state
+                if (CurrentState == GreenhouseState.PROCESSING_DATA)
+                {
+                    // If we're above the temperature limit, extend the shade
+                    // Otherwise, the shade stays retracted
+                    if (value >= HighLimit)
+                    {
+                        return GreenhouseState.SHADING;
+                    }
+                    else
+                    {
+                        CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                        return GreenhouseState.NO_CHANGE;
+                    }
+                }
+                // If we're already shading
+                else if (CurrentState == GreenhouseState.PROCESSING_SHADING)
+                {
+                    // If we're 10 degrees below the high limit now, retract the shade
+                    if (value < HighLimit - 10)
+                    {
+                        return GreenhouseState.WAITING_FOR_DATA;
+                    }
+                    // Otherwise, keep the shade extended
+                    else
+                    {
+                        CurrentState = GreenhouseState.SHADING;
+                        return GreenhouseState.NO_CHANGE;
+                    }
+                }
+                // If we're in neither processing state, throw an error
+                else
+                {
+                    return GreenhouseState.ERROR;
+                }
+            }
+            else if (ManualShade == true)
+            {
+                if (CurrentState == GreenhouseState.PROCESSING_SHADING)
+                {
+                    CurrentState = GreenhouseState.SHADING;
+                    return GreenhouseState.NO_CHANGE;
+                }
+                else
+                {
+                    return GreenhouseState.SHADING;
+                }
+            }
+            else if (ManualShade == false)
+            {
+                ManualShade = null;
+                if (CurrentState == GreenhouseState.PROCESSING_DATA)
+                {
+                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                    return GreenhouseState.NO_CHANGE;
+                }
+                else
+                {
+                    return GreenhouseState.WAITING_FOR_DATA;
+                }
+            }
+            else
+            {
+                return GreenhouseState.ERROR;
+            }
+        }
+
+        /// <summary>
         /// Converts a greenhouse state into a set of commands
         /// </summary>
         /// <param name="state"></param>
@@ -54,74 +141,6 @@ namespace GreenhouseController.StateMachines
                 commandsToSend.Add(Commands.SHADE_RETRACT);
             }
             return commandsToSend;
-        }
-
-        /// <summary>
-        /// Determines the state of the shading state machine based on lighting sensor value
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public GreenhouseState DetermineState(double value = 0)
-        {
-            if (CurrentState == GreenhouseState.SHADING)
-            {
-                CurrentState = GreenhouseState.PROCESSING_SHADING;
-            }
-            else
-            {
-                CurrentState = GreenhouseState.PROCESSING_DATA;
-            }
-
-            if (ManualShade != true)
-            {
-                if (value >= HighLimit && CurrentState != GreenhouseState.PROCESSING_SHADING)
-                {
-                    return GreenhouseState.SHADING;
-                }
-                else if (value >= HighLimit && CurrentState == GreenhouseState.PROCESSING_SHADING)
-                {
-                    CurrentState = GreenhouseState.SHADING;
-                    return GreenhouseState.NO_CHANGE;
-                }
-                else if (value < HighLimit && CurrentState == GreenhouseState.PROCESSING_DATA)
-                {
-                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
-                    return GreenhouseState.NO_CHANGE;
-                }
-                else
-                {
-                    return GreenhouseState.WAITING_FOR_DATA;
-                }
-            }
-            else if (ManualShade == true)
-            {
-                if (CurrentState == GreenhouseState.PROCESSING_SHADING)
-                {
-                    CurrentState = GreenhouseState.SHADING;
-                    return GreenhouseState.NO_CHANGE;
-                }
-                else
-                {
-                    return GreenhouseState.SHADING;
-                }
-            }
-            //else if (ManualShade == false)
-            //{
-            //    ManualShade = null;
-            //    if (CurrentState == GreenhouseState.PROCESSING_DATA)
-            //    {
-            //        CurrentState = GreenhouseState.WAITING_FOR_DATA;
-            //        return GreenhouseState.NO_CHANGE;
-            //    }
-            //    else
-            //    {
-            //        return GreenhouseState.WAITING_FOR_DATA;
-            //    }
-            //}
-            else
-            {
-                return GreenhouseState.ERROR;
-            }
         }
 
         /// <summary>
