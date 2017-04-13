14d8a41907e30e8c6c46f1adba185ac529b3ae41
Aaron Roby <aroby@ycp.edu>
2017-04-13 15:20:30 -0400

    Refactored state machine and added unit tests for decision making

--- GreenhouseController/GreenhouseController/StateMachines/TemperatureStateMachine.cs
+++ GreenhouseController/GreenhouseController/StateMachines/TemperatureStateMachine.cs
@@ -8,6 +8,8 @@ namespace GreenhouseController
 {
     public class TemperatureStateMachine : IStateMachine
     {
+        // TODO: separate exhaust and circulating fans!
+        // Circulating fans need to be on for both heating AND cooling!
         private const int _emergencyHigh = 120;
         private const int _emergencyLow = 32;
         // True = on, false = off
@@ -53,10 +55,9 @@ namespace GreenhouseController
         /// </summary>
         /// <param name="value">Average value of temperature data from each zone</param>
         /// <returns></returns>
-        public GreenhouseState DetermineState(double value = 0)
+        public GreenhouseState DetermineState(double value)
         {
-            // TODO: Should we get rid off manually forcing this into the wating for data state when manual control is turned off?
-            // it seems logical to immediately return to automation
+            // TODO: revamp!
             // Determine which processing state we're in
             if (CurrentState == GreenhouseState.HEATING)
             {
@@ -72,113 +73,198 @@ namespace GreenhouseController
             }
            
             // If the state machine isn't in manual control mode
-            if (ManualHeat != true && ManualCool != true)
+            if (ManualHeat == null && ManualCool == null)
             {
-                // Determine what state to return/change to
-                // If we're coming from an action state and we meet action criteria,
-                // go back to the action state
-                // If we're lower than the low limit
-                if (value <= LowLimit && CurrentState != GreenhouseState.PROCESSING_HEATING)
+                // If we're coming from the wait state
+                if (CurrentState == GreenhouseState.PROCESSING_DATA)
                 {
-                    if (value <= _emergencyLow)
-                    {
-                        return GreenhouseState.EMERGENCY;
-                    }
-                    else
-                    {
-                        return GreenhouseState.HEATING;
-                    }
-                }
-                // If we're lower than the low limit but we're already heating
-                else if (value <= LowLimit && CurrentState == GreenhouseState.PROCESSING_HEATING)
-                {
-                    CurrentState = GreenhouseState.HEATING;
-                    return GreenhouseState.NO_CHANGE;
-                }
-                // If we're higher than the high limit
-                else if (value >= HighLimit && CurrentState != GreenhouseState.PROCESSING_COOLING)
-                {
-                    if (value >= _emergencyHigh)
-                    {
-                        return GreenhouseState.EMERGENCY;
-                    }
-                    else
+                    // If the value is too high but below emergency, we go to cooling
+                    if (value >= HighLimit && value < _emergencyHigh)
                     {
                         return GreenhouseState.COOLING;
                     }
-                }
-                // If we're higher than the high limit but we're already cooling
-                else if (value >= HighLimit && CurrentState == GreenhouseState.PROCESSING_COOLING)
-                {
-                    if (value >= _emergencyHigh)
+                    else if (value >= _emergencyHigh)
                     {
                         return GreenhouseState.EMERGENCY;
                     }
+                    // If the value is too low but above emergency, we go to heating
+                    else if (value <= LowLimit && value > _emergencyLow)
+                    {
+                        return GreenhouseState.HEATING;
+                    }
+                    else if (value <= _emergencyLow)
+                    {
+                        return GreenhouseState.EMERGENCY;
+                    }
+                    // Otherwise, we stay waiting
                     else
+                    {
+                        CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                        return GreenhouseState.NO_CHANGE;
+                    }
+                }
+                // If we're coming from the cooling state
+                else if (CurrentState == GreenhouseState.PROCESSING_COOLING)
+                {
+                    // If the value is still to high, but not at emergency values
+                    if (value >= HighLimit - 5 && value < _emergencyHigh)
                     {
                         CurrentState = GreenhouseState.COOLING;
                         return GreenhouseState.NO_CHANGE;
                     }
+                    // If the temp is above the emergency value
+                    else if (value > _emergencyHigh)
+                    {
+                        return GreenhouseState.EMERGENCY;
+                    }
+                    // If we're within the limits (including hysteresis)
+                    else if (value < HighLimit - 5 && value >= LowLimit)
+                    {
+                        return GreenhouseState.WAITING_FOR_DATA;
+                    }
+                    // If we somehow went below the low limit
+                    else if (value < LowLimit && value > _emergencyLow)
+                    {
+                        return GreenhouseState.HEATING;
+                    }
+                    // If we somehow hit the emergency low limit
+                    else if (value <= _emergencyLow)
+                    {
+                        return GreenhouseState.EMERGENCY;
+                    }
+                    // If we somehow meet none of those criteria, error
+                    else
+                    {
+                        return GreenhouseState.ERROR;
+                    }
                 }
-                // If we're neither too high nor too low 
-                else if (value > LowLimit && value < HighLimit && CurrentState == GreenhouseState.PROCESSING_DATA)
+                else if (CurrentState == GreenhouseState.PROCESSING_HEATING)
                 {
-                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
-                    return GreenhouseState.NO_CHANGE;
+                    // If value is still below low limit but above emergency value
+                    if (value <= LowLimit + 5 && value > _emergencyLow)
+                    {
+                        CurrentState = GreenhouseState.HEATING;
+                        return GreenhouseState.NO_CHANGE;
+                    }
+                    // If the value drops below the emergency low
+                    else if (value <= _emergencyLow)
+                    {
+                        return GreenhouseState.EMERGENCY;
+                    }
+                    // If we're between the high limit and low lim + hysteresis
+                    else if (value >= LowLimit + 5 && value < HighLimit)
+                    {
+                        return GreenhouseState.WAITING_FOR_DATA;
+                    }
+                    // If we somehow hit the high limit but not emergency high
+                    else if (value >= HighLimit && value < _emergencyHigh)
+                    {
+                        return GreenhouseState.COOLING;
+                    }
+                    // If we hit the emergency high limit
+                    else if (value >= _emergencyHigh)
+                    {
+                        return GreenhouseState.EMERGENCY;
+                    }
+                    // If we don't hit any of those conditions, error
+                    else
+                    {
+                        return GreenhouseState.ERROR;
+                    }
                 }
-                else if (value <= HighLimit - 5 && value > LowLimit && CurrentState == GreenhouseState.PROCESSING_COOLING)
-                {
-                    return GreenhouseState.WAITING_FOR_DATA;
-                }
-                else if (value >= LowLimit + 5 && value < HighLimit && CurrentState == GreenhouseState.PROCESSING_HEATING)
-                {
-                    return GreenhouseState.WAITING_FOR_DATA;
-                }
+                // If we're not in any of the processing states, error
                 else
                 {
-                    return GreenhouseState.WAITING_FOR_DATA;
+                    return GreenhouseState.ERROR;
                 }
-                
             }
             // If ManualHeat is on
             else if (ManualHeat == true && ManualCool != true)
             {
+                // If we're already heating, keep heating
                 if (CurrentState == GreenhouseState.PROCESSING_HEATING)
                 {
                     CurrentState = GreenhouseState.HEATING;
                     return GreenhouseState.NO_CHANGE;
                 }
+                // Otherwise, turn on the heat
                 else
                 {
                     return GreenhouseState.HEATING;
                 }
             }
-            // If ManualHeat is off
-            //else if (ManualHeat == false)
-            //{
-            //    ManualHeat = null;
-            //    return GreenhouseState.WAITING_FOR_DATA;
-            //}
-            // If ManualCool is on
+            //If ManualHeat is off
+            else if (ManualHeat == false)
+            {
+                // If we're heating currently, turn it off
+                if (CurrentState == GreenhouseState.PROCESSING_HEATING)
+                {
+                    return GreenhouseState.WAITING_FOR_DATA;
+                }
+                // If we're cooling currently, don't do anything
+                else if (CurrentState == GreenhouseState.PROCESSING_COOLING)
+                {
+                    CurrentState = GreenhouseState.COOLING;
+                    return GreenhouseState.NO_CHANGE;
+                }
+                // If we're doing nothing currently, continue to do nothing
+                else
+                {
+                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                    return GreenhouseState.NO_CHANGE;
+                }
+            }
+            //If ManualCool is on
             else if (ManualCool == true && ManualHeat != true)
             {
+                // If we're already cooling, don't do anything new
                 if (CurrentState == GreenhouseState.PROCESSING_COOLING)
                 {
                     CurrentState = GreenhouseState.COOLING;
                     return GreenhouseState.NO_CHANGE;
                 }
+                // If we're not cooling already, start cooling
                 else
                 {
                     return GreenhouseState.COOLING;
                 }
             }
             // If ManualCool is off
-            //else if (ManualCool == false)
-            //{
-            //    ManualCool = null;
-            //    return GreenhouseState.WAITING_FOR_DATA;
-            //}
+            else if (ManualCool == false)
+            {
+                // If we're cooling currently, go back to waiting
+                if (CurrentState == GreenhouseState.PROCESSING_COOLING)
+                {
+                    return GreenhouseState.WAITING_FOR_DATA;
+                }
+                // If we're heating right now, don't do anything
+                else if (CurrentState == GreenhouseState.PROCESSING_HEATING)
+                {
+                    CurrentState = GreenhouseState.HEATING;
+                    return GreenhouseState.NO_CHANGE;
+                }
+                // If we're doing nothing right now, continue to do nothing
+                else
+                {
+                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                    return GreenhouseState.NO_CHANGE;
+                }
+            }
+            // If both were set to off, go to the waiting state
+            else if (ManualCool == false && ManualHeat == false)
+            {
+                if (CurrentState == GreenhouseState.PROCESSING_DATA)
+                {
+                    CurrentState = GreenhouseState.WAITING_FOR_DATA;
+                    return GreenhouseState.NO_CHANGE;
+                }
+                else
+                {
+                    return GreenhouseState.WAITING_FOR_DATA;
+                }
+            }
             // If we don't meet any of those criteria, return an error state
+            // Such as both heating AND cooling are turn on
             else
             {
                 return GreenhouseState.ERROR;
--- GreenhouseController/GreenhouseUnitTests/TemperatureStateMachineTests.cs
+++ GreenhouseController/GreenhouseUnitTests/TemperatureStateMachineTests.cs
@@ -23,89 +23,300 @@ namespace GreenhouseUnitTests
         {
             testMachine = new TemperatureStateMachine();
             testMachine.HighLimit = 85;
-            testMachine.LowLimit = 55;
+            testMachine.LowLimit = 65;
             GreenhouseState result;
 
-            // Test processing_heating decisions
-            testMachine.CurrentState = GreenhouseState.HEATING;
-            result = testMachine.DetermineState(45);
-            Assert.IsTrue(result == GreenhouseState.NO_CHANGE && testMachine.CurrentState == GreenhouseState.HEATING);
+            #region Automated Decision Tests
+            // Test setup
+            testMachine.ManualCool = null;
+            testMachine.ManualHeat = null;
 
-            result = testMachine.DetermineState((double)testMachine.LowLimit + 5);
-            Assert.IsTrue(result == GreenhouseState.WAITING_FOR_DATA);
+            #region Processing_data Tests
+            // Test case: automated mode
+            // coming from waiting state
+            // value within high and low limits
+            // Result: waiting
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
 
-            // Test processing_cooling decisions
+            result = testMachine.DetermineState(75);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: automated mode
+            // coming from waiting state
+            // value above high limit but below emergency high
+            // Result: cooling
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(100);
+            Assert.IsTrue(result == GreenhouseState.COOLING);
+
+            // Test case: automated mode
+            // coming from waiting state
+            // value above emergency high
+            // Result: emergency
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(150);
+            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+
+            // Test case: automated mode
+            // coming from waiting state
+            // value below low but above emergency low
+            // Result: heating
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(55);
+            Assert.IsTrue(result == GreenhouseState.HEATING);
+
+            // Test case: automated mode
+            // coming from waiting state
+            // value below emergency low
+            // Result: emergency
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(0);
+            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+            #endregion
+            #region Processing_cooling Tests
+            // Test case: automated mode
+            // coming from cooling state
+            // value high but not above emergency high
+            // Result: no change/cooling
             testMachine.CurrentState = GreenhouseState.COOLING;
-            result = testMachine.DetermineState(90);
-            Assert.IsTrue(result == GreenhouseState.NO_CHANGE && testMachine.CurrentState == GreenhouseState.COOLING);
 
-            result = testMachine.DetermineState((double)testMachine.HighLimit - 5);
+            result = testMachine.DetermineState(100);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.COOLING);
+
+            // Test case: automated mode
+            // coming from cooling state
+            // value not below hysteresis
+            // Result: cooling/no change
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(83);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.COOLING);
+
+            // Test case: automated mode
+            // coming from cooling state
+            // value above emergency high
+            // Result: emergency
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(150);
+            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+
+            // Test case: automated mode
+            // coming form cooling state
+            // value within limits w/ hysteresis
+            // Result : waiting
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(75);
             Assert.IsTrue(result == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: automated mode
+            // coming from cooling state
+            // value below low limit but not emergency low
+            // Result: heating
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(45);
+            Assert.IsTrue(result == GreenhouseState.HEATING);
+
+            // Test case: automated mode
+            // coming from cooling state
+            // value below emergency low
+            // Result: emergency
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(0);
+            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+            #endregion
+            #region Processing_heating Tests
+            // Test case: automated mode
+            // coming from heating state
+            // value still below low but above emergency
+            // Result: heating/no change
+            testMachine.CurrentState = GreenhouseState.HEATING;
+
+            result = testMachine.DetermineState(45);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.HEATING);
             
-            // Test processing_data decisions
-            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
-            result = testMachine.DetermineState(45);
-            Assert.IsTrue(result == GreenhouseState.HEATING);
-
-            result = testMachine.DetermineState(95);
-            Assert.IsTrue(result == GreenhouseState.COOLING);
-
-            result = testMachine.DetermineState(73);
-            Assert.IsTrue(result == GreenhouseState.NO_CHANGE && testMachine.CurrentState == GreenhouseState.WAITING_FOR_DATA);
-
-            // Test for error conditions
-            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
-            result = testMachine.DetermineState(150);
-            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
-
-            result = testMachine.DetermineState(0);
-            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
-
+            // Test case: automated mode
+            // coming from heating state
+            // value below emergency low
+            // Result: emergency
             testMachine.CurrentState = GreenhouseState.HEATING;
-            result = testMachine.DetermineState(150);
-            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
 
             result = testMachine.DetermineState(0);
             Assert.IsTrue(result == GreenhouseState.EMERGENCY);
 
-            testMachine.CurrentState = GreenhouseState.COOLING;
-            result = testMachine.DetermineState(150);
-            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+            // Test case: automated mode
+            // coming from heating state
+            // value between high lim and low lim w/ hysteresis
+            // Result: waiting
+            testMachine.CurrentState = GreenhouseState.HEATING;
 
-            result = testMachine.DetermineState(0);
-            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+            result = testMachine.DetermineState(75);
+            Assert.IsTrue(result == GreenhouseState.WAITING_FOR_DATA);
 
-            // Test manual control
-            // Test manual cooling
-            testMachine.ManualCool = true;
-            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
-            result = testMachine.DetermineState(80);
+            // Test case: automated mode
+            // coming from heating state
+            // value above low lim, below hysteresis value
+            // Result: heating
+            testMachine.CurrentState = GreenhouseState.HEATING;
+
+            result = testMachine.DetermineState(68);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.HEATING);
+
+            // Test case: automated mode
+            // coming from heating state
+            // value above high lim but below emergency high
+            // Result: cooling
+            testMachine.CurrentState = GreenhouseState.HEATING;
+
+            result = testMachine.DetermineState(100);
             Assert.IsTrue(result == GreenhouseState.COOLING);
 
-            testMachine.CurrentState = GreenhouseState.COOLING;
-            result = testMachine.DetermineState(80);
-            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            // Test case: automated mode
+            // coming from heating state
+            // value above emergency high
+            // Result: emergency
+            testMachine.CurrentState = GreenhouseState.HEATING;
 
-            // Test when both manual heat and cooling are true
+            result = testMachine.DetermineState(160);
+            Assert.IsTrue(result == GreenhouseState.EMERGENCY);
+            #endregion
+            #endregion
+            #region Manual Mode Tests
+            // Setup
             testMachine.ManualHeat = true;
-            result = testMachine.DetermineState(75);
-            Assert.IsTrue(result == GreenhouseState.ERROR);
 
-            testMachine.ManualCool = false;
+            // Test case: manual heat on
+            // coming from heat state
+            // Result: heating/no change
+            testMachine.CurrentState = GreenhouseState.HEATING;
 
-            // Test manual heating
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.HEATING);
+
+            // Test case: manual heat on
+            // coming from wait state
+            // Result: heating
             testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
-            result = testMachine.DetermineState(75);
+
+            result = testMachine.DetermineState(70);
             Assert.IsTrue(result == GreenhouseState.HEATING);
 
+            // Test case: manual heat on
+            // coming from cool state
+            // Result: heating
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.HEATING);
+
+            // Test case: manual heat off
+            // coming from heat state
+            // Result: waiting
+            testMachine.ManualHeat = false;
             testMachine.CurrentState = GreenhouseState.HEATING;
-            result = testMachine.DetermineState(75);
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: manual heat off
+            // coming from wait state
+            // Result: waiting
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(70);
             Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: manual heat off
+            // coming from cooling state
+            // Result: waiting
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.COOLING);
+
+            // Test case: manual cool on
+            // coming from wait state
+            // Result: cooling
+            testMachine.ManualCool = true;
+            testMachine.ManualHeat = null;
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.COOLING);
+
+            // Test case: manual cool on
+            // coming from heat state
+            // Result: cooling
+            testMachine.CurrentState = GreenhouseState.HEATING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.COOLING);
+
+            // Test case: manual cool on
+            // coming from cool state
+            // Result: cooling/no change
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.COOLING);
+
+            // Test case: manual cool off
+            // coming from cool state
+            // Result: waiting
+            testMachine.ManualCool = false;
+            testMachine.CurrentState = GreenhouseState.COOLING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: manual cool off
+            // coming from heat state
+            // Result: heating/no change
+            testMachine.CurrentState = GreenhouseState.HEATING;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.HEATING);
+
+            // Test case: manual cool off
+            // coming from wait state
+            // Result: waiting/no change
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.NO_CHANGE);
+            Assert.IsTrue(testMachine.CurrentState == GreenhouseState.WAITING_FOR_DATA);
+
+            // Test case: both manual cool and heat on
+            testMachine.CurrentState = GreenhouseState.WAITING_FOR_DATA;
+            testMachine.ManualCool = true;
+            testMachine.ManualHeat = true;
+
+            result = testMachine.DetermineState(70);
+            Assert.IsTrue(result == GreenhouseState.ERROR);
+            #endregion
         }
 
         [TestMethod]
         public void TestConvertTemperatureStateToCommands()
         {
+            // TODO: implement this!
             throw new NotImplementedException();
         }
     }
