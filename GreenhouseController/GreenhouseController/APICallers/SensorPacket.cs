ad4fa46c4ad2fa8ffa227543d9476da03ae64b81
Aaron <aroby@ycp.edu>
2017-04-25 20:38:20 -0400

    Initial commit

---
+++ GreenhouseController/GreenhouseController/APICallers/SensorPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenhouseController.APICallers
+{
+    public class SensorPacket
+    {
+        public DateTime sampletime;
+        public int zone;
+        double light;
+        double temperature;
+        double humidity;
+        double probe1;
+        double probe2;
+    }
+}
